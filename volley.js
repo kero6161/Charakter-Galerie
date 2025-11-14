3e5c560cf643c48b07d379b4db24e44c0114aa27
kero6161 <kerouchiha61@gmail.com>
2025-11-14 20:41:26 +0100

    Update volley.js

--- volley.js
+++ volley.js
@@ -6,7 +6,7 @@ function presstext(name) {
     const characters = {
         bo: { img: "bokuto.png", text: "Bokuto Koutarou – ein As aus Haikyuu!!, voller Energie und extrem stark." },
         go: { img: "goku.jpeg", text: "Son Goku – Saiyajin-Krieger aus Dragon Ball, liebt Kämpfe und Training." },
-        sa: { img: "sanemi.jpg", text: "Sanemi Shinazugawa – Wind-Säule aus Demon Slayer, sehr temperamentvoll." },
+        sh: { img: "shankss.jpg", text: "Goat." },
         hat: { img: "hatori.jpg", text: "Hatori – ruhiger und geheimnisvoller Charakter, oft sehr analytisch." },
         lu: { img: "luffy.jpg", text: "Monkey D. Luffy – zukünftiger Piratenkönig, isst gerne Fleisch und ist dehnbar." },
         la: { img: "law.jpg", text: "Trafalgar Law – Chirurg des Todes, extrem intelligent und strategisch." }
@@ -28,3 +28,4 @@ function presstext(name) {
     allButtons.forEach(btn => btn.classList.remove("active"));
     event.target.classList.add("active");
 }
+
